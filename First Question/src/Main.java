72e6d369d89331f71a46c8f41d76d468f23ef45d
Abdalrhman-Jabri <155806847+Abdalrhman-Jabri@users.noreply.github.com>
2024-06-09 19:34:54 +0300

    Update Main.java

--- First Question/src/Main.java
+++ First Question/src/Main.java
@@ -20,6 +20,73 @@ public class Main {
         BT.Export("textFiles/Data2.txt");
     }
 
+    public static void four() {
+        List<Rectangle> smallRects = new ArrayList<>();
+        smallRects.add(new Rectangle(20, 10));
+        smallRects.add(new Rectangle(20, 10));
+        smallRects.add(new Rectangle(30, 10));
+        smallRects.add(new Rectangle(30, 50));
+        smallRects.add(new Rectangle(40, 30));
+        smallRects.add(new Rectangle(40, 20));
+
+        int totalSmallArea = smallRects.stream()
+                .mapToInt(rect -> rect.width * rect.height)
+                .sum();
+
+        boolean foundSolution = false;
+        for (int i = 1; i <= totalSmallArea; i++) {
+            if (totalSmallArea % i == 0) {
+                Packer packer = new Packer(i, totalSmallArea / i);
+                if (packer.fit(smallRects)) {
+                    System.out.println("The small rectangles can fill the big rectangle with dimensions " + i + "x"
+                            + (totalSmallArea / i));
+                    foundSolution = true;
+                    break;
+                }
+            }
+        }
+
+        if (!foundSolution) {
+            System.out.println("The small rectangles cannot fill any big rectangle.");
+        }
+    }
+
+    public static void five() {
+        List<Rectangle> smallRects = new ArrayList<>();
+        smallRects.add(new Rectangle(20, 10));
+        smallRects.add(new Rectangle(20, 10));
+        smallRects.add(new Rectangle(30, 10));
+        smallRects.add(new Rectangle(30, 50));
+        smallRects.add(new Rectangle(40, 30));
+        smallRects.add(new Rectangle(40, 20));
+
+        int totalSmallArea = smallRects.stream()
+                .mapToInt(rect -> rect.width * rect.height)
+                .sum();
+
+        for (int i = 1; i <= totalSmallArea; i++) {
+            if (totalSmallArea % i == 0) {
+                Packer_1 packer = new Packer_1(i, totalSmallArea / i);
+                List<List<Packer_1.Placement>> solutions = packer.fit(smallRects);
+                if (solutions.size() > 0) {
+                    System.out.println(
+                            "The small rectangles can fill the big rectangle in the following ways for the width " + i
+                                    + " and height " + (totalSmallArea / i));
+                    for (int solutionIndex = 0; solutionIndex < solutions.size(); solutionIndex++) {
+                        System.out.println("Solution " + (solutionIndex + 1) + ":");
+                        for (Packer_1.Placement placement : solutions.get(solutionIndex)) {
+                            System.out.println("Rectangle " + placement.rect.width + "x" + placement.rect.height
+                                    + " at (" + placement.x + ", " + placement.y + ")");
+                        }
+                    }
+                } else {
+                    System.out.println("The small rectangles cannot fill the big rectangle for the width " + i
+                            + " and height " + (totalSmallArea / i));
+                }
+            }
+        }
+    }
+
     public static void seven() {
         BinaryTree bt = new BinaryTree();
         JFrame frame = new JFrame();
